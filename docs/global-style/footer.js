4be0ee0906d14439540a78c1a6444bccf8498548
Jack Scales <jack@myexoticfruit.com>
2026-09-02 13:17:08 +0100

    chore: add tawk.to chore: found uncropped events posters

---
+++ docs/global-style/footer.js
@@ -0,0 +1,16 @@
+// tawk.to script!
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5c865d4cc37db86fcfcd2077/1jshgfl56';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+})();
+
+Tawk_API.onLoad = function () {
+    Tawk_API.setAttributes({
+        site: "The Greengrocer"
+    });
+};
